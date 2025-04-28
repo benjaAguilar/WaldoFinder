7874dbdf238a120332291f32067bdd605dc0c418
ColorCode <benjaaguilar02@gmail.com>
2025-04-28 14:08:02 -0300

    feat: add waldo cards on home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,15 @@
+import { WaldoCard } from "../components/WaldoCard";
+
 export function Home() {
   return (
-    <div>
+    <div className="home">
       <h1>Waldo finder</h1>
       <p>Lets find waldo</p>
+      <div className="grid">
+        <WaldoCard img="/waldoBeach.webp" title="Waldo on the beach" />
+        <WaldoCard img="/WaldoAndWarriors.webp" title="Waldo and warriors" />
+        <WaldoCard img="/waldodragons.webp" title="Waldo and dragons" />
+      </div>
       <a href="/game">Play</a>
     </div>
   );
