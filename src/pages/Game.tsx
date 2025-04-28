0c2a1464f3f1a34423656af06e186892c00c18bd
ColorCode <benjaaguilar02@gmail.com>
2025-04-28 14:06:50 -0300

    feat: add game calculations and logic

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,18 +1,10 @@
 import { useEffect, useRef, useState } from "react";
-
-const originalImage = {
-  coords: {
-    x1: 458,
-    y1: 1563,
-    x2: 487,
-    y2: 1527,
-  },
-  width: 2828,
-  height: 1828,
-};
+import mockDB from "../mockDB.json";
 
 export function Game() {
   const imageRef = useRef<HTMLImageElement | null>(null);
+  const crossElement = useRef<HTMLDivElement | null>(null);
+  const [waldoGame, setWaldoGame] = useState(mockDB.waldoBeach);
   const [actualCoords, setActualCoords] = useState({
     x1: 0,
     y1: 0,
@@ -22,9 +14,18 @@ export function Game() {
 
   useEffect(() => {
     window.addEventListener("resize", calculateCoords);
-    return () => window.removeEventListener("resize", calculateCoords);
-  }, []);
 
+    const game = localStorage.getItem("game");
+    if (game === "Waldo and warriors") {
+      setWaldoGame(mockDB.waldoWarriors);
+    } else if (game === "Waldo and dragons") {
+      setWaldoGame(mockDB.waldoDragons);
+    }
+
+    return () => window.removeEventListener("resize", calculateCoords);
+  }, [calculateCoords]);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   function calculateCoords() {
     if (!imageRef.current) return;
 
@@ -32,16 +33,16 @@ export function Game() {
     console.log(imageData);
     setActualCoords({
       x1:
-        (originalImage.coords.x1 * imageData.width) / originalImage.width +
+        (waldoGame.coords.x1 * imageData.width) / waldoGame.width +
         imageData.left,
       x2:
-        (originalImage.coords.x2 * imageData.width) / originalImage.width +
+        (waldoGame.coords.x2 * imageData.width) / waldoGame.width +
         imageData.left,
       y1:
-        (originalImage.coords.y1 * imageData.height) / originalImage.height +
+        (waldoGame.coords.y1 * imageData.height) / waldoGame.height +
         imageData.top,
       y2:
-        (originalImage.coords.y2 * imageData.height) / originalImage.height +
+        (waldoGame.coords.y2 * imageData.height) / waldoGame.height +
         imageData.top,
     });
   }
@@ -49,6 +50,7 @@ export function Game() {
   function getCoords(e: React.MouseEvent<HTMLImageElement>) {
     console.log(actualCoords);
     console.log(`x: ${e.clientX} y: ${e.clientY}`);
+    let waldoIsFound = false;
 
     const adjustedY1 = actualCoords.y1 - window.scrollY;
     const adjustedY2 = actualCoords.y2 - window.scrollY;
@@ -59,8 +61,29 @@ export function Game() {
       adjustedY2 <= e.clientY &&
       e.clientY <= adjustedY1
     ) {
+      waldoIsFound = true;
       console.log("You found Waldo!");
       alert("You found Waldo!");
+    }
+
+    if (crossElement.current) {
+      if (waldoIsFound) {
+        crossElement.current.firstChild.textContent = "✔";
+        crossElement.current.style.color = "greenyellow";
+        crossElement.current.style.borderColor = "greenyellow";
+      }
+
+      crossElement.current.style.display = "flex";
+      const width = crossElement.current.offsetWidth;
+      const height = crossElement.current.offsetHeight;
+      crossElement.current.style.top = `${
+        e.clientY + window.scrollY - height / 2
+      }px`;
+      crossElement.current.style.left = `${e.clientX - width / 2}px`;
+
+      setTimeout(() => {
+        crossElement.current.style.display = "none";
+      }, 2000);
     }
   }
 
@@ -68,12 +91,15 @@ export function Game() {
     <>
       <img
         className="waldoImage"
-        src="/WaldoAndWarriors.webp"
+        src={waldoGame.img}
         alt="Waldo's game"
         ref={imageRef}
         onClick={getCoords}
         onLoad={calculateCoords}
       />
+      <div className="cross" ref={crossElement}>
+        <div>✘</div>
+      </div>
     </>
   );
 }
