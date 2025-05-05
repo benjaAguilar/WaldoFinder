4f348404a1302286678db24e9f1834ae555686ea
ColorCode <benjaaguilar02@gmail.com>
2025-05-05 16:38:34 -0300

    feat: add post /user on waldo card

--- src/components/WaldoCard.tsx
+++ src/components/WaldoCard.tsx
@@ -1,21 +1,44 @@
-interface props {
-  img: string;
-  title: string;
-}
+import { useNavigate } from "react-router-dom";
+import { waldoCardProps } from "../types/waldoImages";
+import { fetchData } from "../utils/fetchData";
 
-export function WaldoCard({ img, title }: props) {
-  function setGame() {
-    localStorage.setItem("game", title);
+export function WaldoCard({ imageData }: waldoCardProps) {
+  const navigate = useNavigate();
+
+  async function setGame(leaderboardId: number) {
+    const user = await fetchData(
+      "/user",
+      "POST",
+      {
+        "Content-Type": "application/json",
+      },
+      JSON.stringify({ leaderboardId })
+    );
+
+    if (!user.success) {
+      return alert(user.message);
+    }
+
+    console.log("user created!");
+    console.log(user);
+
+    // enviar al usuario a jugar el juego elegido, incluyendo toda la data
+    navigate("/game", { state: imageData });
   }
 
   return (
     <div className="card">
-      <img src={img} alt="waldo image" />
+      <img src={imageData.imgRoute} alt={`${name} image`} />
       <div className="data">
-        <h3>{title}</h3>
-        <a className="playB" href="/game" onClick={setGame}>
+        <h3>{imageData.name}</h3>
+        <button
+          className="playB"
+          onClick={() => {
+            setGame(imageData.leaderboard.id);
+          }}
+        >
           Play
-        </a>
+        </button>
         <button>Leaderboard</button>
       </div>
     </div>
