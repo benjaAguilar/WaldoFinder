b4049995372b7c8b9d31b8253c4564745871dc3d
ColorCode <benjaaguilar02@gmail.com>
2025-05-05 16:35:13 -0300

    feat: add fetch to get waldo images

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,33 @@
+import { useEffect, useState } from "react";
 import { WaldoCard } from "../components/WaldoCard";
+import { fetchData } from "../utils/fetchData";
+import { ImagesDataArr } from "../types/waldoImages";
 
 export function Home() {
+  const [images, setImages] = useState<ImagesDataArr | null>(null);
+
+  useEffect(() => {
+    async function getWaldoImages() {
+      const images = await fetchData("/image", "GET");
+
+      if (images.success) setImages(images);
+    }
+
+    getWaldoImages();
+  }, []);
+
   return (
     <div className="home">
       <h1>Waldo finder</h1>
       <p>Lets find waldo</p>
       <div className="grid">
-        <WaldoCard img="/waldoBeach.webp" title="Waldo on the beach" />
-        <WaldoCard img="/WaldoAndWarriors.webp" title="Waldo and warriors" />
-        <WaldoCard img="/waldodragons.webp" title="Waldo and dragons" />
+        {images ? (
+          images.images.map((image) => {
+            return <WaldoCard imageData={image} key={image.id} />;
+          })
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
     </div>
   );
