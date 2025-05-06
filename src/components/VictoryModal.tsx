d98b1b87e6c0d575fc9fb8a009180302a73e5062
ColorCode <benjaaguilar02@gmail.com>
2025-05-06 11:29:00 -0300

    feat: add submitForm logic

--- src/components/VictoryModal.tsx
+++ src/components/VictoryModal.tsx
@@ -1,14 +1,57 @@
+import { useRef } from "react";
+import { fetchData } from "../utils/fetchData";
+import { useNavigate } from "react-router-dom";
+
 interface modalProps {
   time: string;
 }
 
 export function VictoryModal({ time }: modalProps) {
+  const navigate = useNavigate();
+  const formRef = useRef<HTMLFormElement | null>(null);
+
+  async function putUsername(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!formRef.current) {
+      return alert("Error sending the form");
+    }
+
+    const formData = new FormData(formRef.current);
+    const params = new URLSearchParams();
+
+    for (const [key, value] of formData.entries()) {
+      if (typeof value === "string") {
+        params.append(key, value);
+      }
+    }
+
+    const user = await fetchData(
+      "/user/username",
+      "PUT",
+      { "Content-Type": "application/x-www-form-urlencoded" },
+      params.toString()
+    );
+
+    if (!user.success) {
+      return alert(user.message);
+    }
+
+    alert("score saved!");
+    navigate("/");
+  }
+
   return (
     <div className="modal">
       <button>✘</button>
       <h2>You Found Waldo!</h2>
       <p>time: {time}</p>
-      <form method="put">
+      <form
+        ref={formRef}
+        onSubmit={(e) => {
+          putUsername(e);
+        }}
+      >
         <label htmlFor="username">Username</label>
         <div>
           <i>Add an username to submit your score*</i>
