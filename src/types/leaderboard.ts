957bf2bac25141669711cee4ba633de77a676b41
ColorCode <benjaaguilar02@gmail.com>
2025-05-06 11:31:50 -0300

    feat: add leaderboard with users data type

--- src/types/leaderboard.ts
+++ src/types/leaderboard.ts
@@ -1,5 +1,12 @@
+import { definedUser } from "./users";
+
 export interface leaderboard {
   id: number;
   name: string;
   imageDataId: string;
+}
+
+export interface leaderboardUsersData {
+  leaderboard: leaderboard;
+  users: definedUser[];
 }
